89a3fd636831ee68488f6990839d331870217d84
J4C0B3Y <J4C0B3Y@gmail.com>
2023-09-11 13:43:01 +1000

    Fix enchanting handler

--- src/main/java/net/j4c0b3y/tridentbuff/TridentBuff.java
+++ src/main/java/net/j4c0b3y/tridentbuff/TridentBuff.java
@@ -1,14 +1,17 @@
 package net.j4c0b3y.tridentbuff;
 
+import lombok.Getter;
 import net.j4c0b3y.tridentbuff.listeners.AnvilListener;
 import net.j4c0b3y.tridentbuff.listeners.DamageListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-@SuppressWarnings("UnusedDeclaration")
 public class TridentBuff extends JavaPlugin {
+    @Getter private static TridentBuff instance;
 
     @Override
     public void onEnable() {
+        instance = this;
+
         getServer().getPluginManager().registerEvents(new AnvilListener(), this);
         getServer().getPluginManager().registerEvents(new DamageListener(), this);
     }
--- src/main/java/net/j4c0b3y/tridentbuff/listeners/AnvilListener.java
+++ src/main/java/net/j4c0b3y/tridentbuff/listeners/AnvilListener.java
@@ -1,33 +1,65 @@
 package net.j4c0b3y.tridentbuff.listeners;
 
+import net.j4c0b3y.tridentbuff.TridentBuff;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 
 public class AnvilListener implements Listener {
 
+    TridentBuff plugin = TridentBuff.getInstance();
+
     @EventHandler
     public void onPrepare(PrepareAnvilEvent event) {
-        ItemStack trident = event.getInventory().getItem(0);
-        ItemStack book = event.getInventory().getItem(1);
+        ItemStack first = event.getInventory().getItem(0);
+        ItemStack second = event.getInventory().getItem(1);
 
-        if (trident == null || book == null) return;
-        if (trident.getItemMeta() == null || book.getItemMeta() == null) return;
-        if (!trident.getType().equals(Material.TRIDENT) || !book.getType().equals(Material.ENCHANTED_BOOK)) return;
+        if (!isType(first, Material.TRIDENT)) return;
+        if (!isType(second, Material.TRIDENT) && !isType(second, Material.ENCHANTED_BOOK)) return;
 
-        int tridentLevel = trident.getEnchantmentLevel(Enchantment.DAMAGE_ALL);
-        int bookLevel = book.getEnchantmentLevel(Enchantment.DAMAGE_ALL);
+        ItemStack result = first.clone();
 
-        if (bookLevel == 0) return;
+        int level = getLevel(first, second, Enchantment.DAMAGE_ALL);
+        if (level > 0) result.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, level);
 
-        int level = Math.max(bookLevel, tridentLevel);
-        if (tridentLevel == bookLevel && level < 5) level++;
+        if (isType(second, Material.TRIDENT)) {
+            Damageable trident = (Damageable) result.getItemMeta();
+            Damageable meta = (Damageable) result.getItemMeta();
 
-        trident.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, level);
+            if (trident != null && meta != null) {
+                meta.setDamage(trident.getDamage() + meta.getDamage());
+            }
+        }
 
-        event.setResult(trident);
+        if (first.isSimilar(result)) return;
+
+        Bukkit.getScheduler().runTask(plugin, () -> event.getInventory().setRepairCost(level));
+        event.setResult(result);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private int getLevel(ItemStack first, ItemStack second, Enchantment enchantment) {
+        int firstLevel = first.getEnchantmentLevel(enchantment);
+        int secondLevel = second.getEnchantmentLevel(enchantment);
+
+        if (isType(second, Material.ENCHANTED_BOOK)) {
+            EnchantmentStorageMeta meta = (EnchantmentStorageMeta) second.getItemMeta();
+            if (meta != null) secondLevel = meta.getStoredEnchantLevel(enchantment);
+        }
+
+        int level = Math.max(firstLevel, secondLevel);
+        if (firstLevel == secondLevel && level < enchantment.getMaxLevel() && level > 0) level++;
+
+        return level;
+    }
+
+    private boolean isType(ItemStack item, Material material) {
+        return item != null && item.getItemMeta() != null && item.getType().equals(material);
     }
 }
