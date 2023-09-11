0de77d0dd42a7d38861f061312c8ace20f9ff2f3
J4C0B3Y <J4C0B3Y@gmail.com>
2023-09-11 11:36:23 +1000

    Clean up main class

--- src/main/java/net/j4c0b3y/tridentbuff/TridentBuff.java
+++ src/main/java/net/j4c0b3y/tridentbuff/TridentBuff.java
@@ -4,14 +4,11 @@ import net.j4c0b3y.tridentbuff.listeners.AnvilListener;
 import net.j4c0b3y.tridentbuff.listeners.DamageListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("UnusedDeclaration")
 public class TridentBuff extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        registerListeners();
-    }
-
-    private void registerListeners() {
         getServer().getPluginManager().registerEvents(new AnvilListener(), this);
         getServer().getPluginManager().registerEvents(new DamageListener(), this);
     }
